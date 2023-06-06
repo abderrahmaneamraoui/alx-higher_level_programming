040d9f32f4fb298baae4394a2e428428a067a489
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-06 13:45:45 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i != j:
-            print("{:d}{:d}".format(i, j), end='')
-            if i < 8:
-                print(", ", end='')
+for i in range(100):
+    print("{:02d}".format(i), end='')
+    if i < 99:
+        print(", ", end='')
 
 print()
