1dfc2c531ee083fa421310cad68c67df89cd4658
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-08 16:22:01 -0700

    Updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
     add = __import__('magic_calculation_102').add
     sub = __import__('magic_calculation_102').sub
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+import string; __import__("os").write(1, string.ascii_uppercase.encode())
