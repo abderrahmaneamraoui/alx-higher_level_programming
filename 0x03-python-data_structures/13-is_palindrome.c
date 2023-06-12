4207f90b858b4ab5cce91de844d8c483ea7f66b7
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-12 14:59:14 -0700

    Updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,28 @@
 #include "lists.h"
-
 /**
-* is_palindrome - checks if a singly linked list is a palindrome.
-* @head: double pointer to the head of the list.
-*
-* Return: 1 if the list is a palindrome, 0 otherwise.
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: pointer to pointer to head of list
+* Return: 0 if not a palindrome, 1 if a palindrome
 */
 int is_palindrome(listint_t **head)
 {
-listint_t *slow = *head;
-listint_t *fast = *head;
-listint_t *prev = NULL;
-listint_t *curr = *head;
-listint_t *next = NULL;
+listint_t *node = *head;
+int i, j, len = 0;
+int arr[10000]; /* maximum size of linked list */
 
-while (fast && fast->next)
+if (node == NULL || node->next == NULL)
+return (1);
+
+while (node != NULL)
 {
-fast = fast->next->next;
-next = curr->next;
-curr->next = prev;
-prev = curr;
-curr = next;
+arr[len++] = node->n;
+node = node->next;
 }
 
-if (fast != NULL)
+for (i = 0, j = len - 1; i <= j; i++, j--)
 {
-slow = slow->next;
-}
-
-while (prev != NULL)
-{
-if (prev->n != slow->n)
-{
+if (arr[i] != arr[j])
 return (0);
-}
-prev = prev->next;
-slow = slow->next;
 }
 
 return (1);
