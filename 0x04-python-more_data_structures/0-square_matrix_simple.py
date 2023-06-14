9387d4043e29e9cfbca09dd042c5b588e6d9cab9
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-14 14:31:45 -0700

    mandatory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,19 +1,4 @@
 #!/urs/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[y ** 2 for y in x] for x in matrix]
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-
-print("Original matrix:")
-for row in matrix:
-    print(row)
-
-print("Squared matrix:")
-for row in new_matrix:
-    print(row)
+    new_matrix = [[x**2 for x in row] for row in matrix]
+    return new_matrix
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i, c in enumerate(roman_string):
+        if i+1 < len(roman_string) and roman_dict[roman_string[i]] < roman_dict[roman_string[i+1]]:
+            result -= roman_dict[roman_string[i]]
+        else:
+            result += roman_dict[roman_string[i]]
+    return result
