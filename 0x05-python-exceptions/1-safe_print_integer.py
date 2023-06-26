82f0babe6081d9970edddab2a7ccee8adb3566b0
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-26 13:38:05 -0700

    mandatory

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-try:
-print("{:d}".format(value))
-return True
-except:
-return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
+
