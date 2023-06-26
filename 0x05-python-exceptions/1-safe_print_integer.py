175ea0032c2f8fa1fd3828555dd8b1f3dca925ff
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-26 13:30:39 -0700

    mandatory

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except:
-        return False
+try:
+print("{:d}".format(value))
+return True
+except:
+return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        print()
-        return count
-    except:
-        print()
-        return count
+count = 0
+try:
+for i in range(x):
+if isinstance(my_list[i], int):
+print("{:d}".format(my_list[i]), end="")
+count += 1
+print()
+return count
+except:
+print()
+return count
