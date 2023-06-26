bedf6b1e6240079e23e7d935e4641d4f80a7e5e8
abderrahmaneamraoui <amraouiabdrahim119@gmail.com>
2023-06-26 14:31:36 -0700

    advanced

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    if a < b:
-        return a + b
-    else:
-        return a * b
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
